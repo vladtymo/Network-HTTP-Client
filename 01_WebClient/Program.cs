e41ff89aa07461a52b80136dbfc23cf95c9659de
vladtymo <tymo.vlad@gmail.com>
2023-06-17 10:34:08 +0300

    Downloader example.

--- 01_WebClient/Program.cs
+++ 01_WebClient/Program.cs
@@ -13,14 +13,14 @@ namespace _01_WebClient
     class Program
     {
         static string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        const string imageUri = @"https://www.atlanticcouncil.org/wp-content/uploads/2021/12/2021-12-16T122128Z_1435296858_RC2KFR903WZL_RTRMADP_3_UKRAINE-CITYSCAPE-scaled-e1640800288914.jpg";
+        const string imageUri = @"https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_640.png";
         const string image2Uri = @"https://ukrainetrek.com/images/rivne-ukraine-oblast-views-10.jpg";
         static async Task Main(string[] args)
         {
             // -=-=-=-=-=-=-=-=-=-=- Donwload File using HttpClient -=-=-=-=-=-=-=-=-=-=-
-            HttpClient httpClient = new HttpClient();
+            //HttpClient httpClient = new HttpClient();
 
-            /////////////// variant 1
+            ///////////////// variant 1
             //HttpRequestMessage message = new HttpRequestMessage()
             //{
             //    Method = HttpMethod.Get,
@@ -35,16 +35,16 @@ namespace _01_WebClient
             //}
 
             /////////////// variant 2
-            byte[] data = await httpClient.GetByteArrayAsync(image2Uri);
-            File.WriteAllBytes($@"{desktopPath}/image.jpg", data);
+            //byte[] data = await httpClient.GetByteArrayAsync(image2Uri);
+            //File.WriteAllBytes($@"{desktopPath}/image.jpg", data);
 
-            // -=-=-=-=-=-=-=-=-=-=- Donwload File using WebClient -=-=-=-=-=-=-=-=-=-=-
-            WebClient webClient = new WebClient();
+            //// -=-=-=-=-=-=-=-=-=-=- Donwload File using WebClient -=-=-=-=-=-=-=-=-=-=-
+            //WebClient webClient = new WebClient();
 
-            // sync download
-            webClient.DownloadFile(imageUri, $@"{desktopPath}\picture.jpg");
+            //// sync download
+            //webClient.DownloadFile(image2Uri, $@"{desktopPath}\picture.jpg");
 
-            Console.WriteLine("File loaded");
+            //Console.WriteLine("File loaded");
 
             #region Open Stream
             //string address = "https://media.inquirer.com/storage/inquirer/projects/year-in-pictures-2019/photos/POY2019_RedC.JPG";
@@ -82,9 +82,9 @@ namespace _01_WebClient
             #endregion
 
             // async download
-            Console.WriteLine("File loading...");
-            DownloadFileAsync("http://212.183.159.230/5MB.zip");
-            DownloadFileAsync("http://212.183.159.230/10MB.zip");
+            //Console.WriteLine("File loading...");
+            //DownloadFileAsync("http://212.183.159.230/5MB.zip");
+            //DownloadFileAsync("http://212.183.159.230/10MB.zip");
 
             Console.WriteLine("Continue...");
             Console.Read();
