2e4be9f947eeef0d4fbcb8970eb68e6bd9949302
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-10-02 16:09:33 +0300

    Update example.

--- 01_WebClient/Program.cs
+++ 01_WebClient/Program.cs
@@ -15,6 +15,8 @@ namespace _01_WebClient
         static string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
         const string imageUri = @"https://www.atlanticcouncil.org/wp-content/uploads/2021/12/2021-12-16T122128Z_1435296858_RC2KFR903WZL_RTRMADP_3_UKRAINE-CITYSCAPE-scaled-e1640800288914.jpg";
         const string image2Uri = @"https://ukrainetrek.com/images/rivne-ukraine-oblast-views-10.jpg";
+        const string image3Uri = @"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg";
+
         static async Task Main(string[] args)
         {
             // -=-=-=-=-=-=-=-=-=-=- Donwload File using HttpClient -=-=-=-=-=-=-=-=-=-=-
@@ -35,16 +37,16 @@ namespace _01_WebClient
             //}
 
             /////////////// variant 2
-            byte[] data = await httpClient.GetByteArrayAsync(image2Uri);
-            File.WriteAllBytes($@"{desktopPath}/image.jpg", data);
+            //byte[] data = await httpClient.GetByteArrayAsync(image2Uri);
+            //File.WriteAllBytes($@"{desktopPath}/image.jpg", data);
 
             // -=-=-=-=-=-=-=-=-=-=- Donwload File using WebClient -=-=-=-=-=-=-=-=-=-=-
             WebClient webClient = new WebClient();
 
             // sync download
-            webClient.DownloadFile(imageUri, $@"{desktopPath}\picture.jpg");
+            //webClient.DownloadFile(imageUri, $@"{desktopPath}\picture.jpg");
 
-            Console.WriteLine("File loaded");
+            //Console.WriteLine("File loaded");
 
             #region Open Stream
             //string address = "https://media.inquirer.com/storage/inquirer/projects/year-in-pictures-2019/photos/POY2019_RedC.JPG";
